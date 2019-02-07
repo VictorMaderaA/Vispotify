769ef2fc656bcf541d74688f10699cf8cdc7466b
VictorMaderaA <victor.madera.a@gmail.com>
2019-02-07 18:03:58 +0100

    Arreglos

--- src/App.js
+++ src/App.js
@@ -3,41 +3,43 @@ import './App.css';
 import queryString from 'query-string';
 import YoutubePlayer from './YoutubePlayer.js';
 import { getUserData, getCurrentSongData}  from './SpotifyManager';
+import { stringify } from 'querystring';
 
 
 let defaultStyle = {
   color: '#fff'
 };
 
+let dataPlis;
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      serverData: {},
-      songData: '',
-      videoData: ''
+      user_display_name: null,
+      user_email: null,
+      user_id: null,
+      user_image_url: null,
+
+      songData: {}
     }
 
     this.prevSong = {
       name: '',
       artist:''
-    }
-
-    this.userData = {};
-    this.songData = {};
-    
+    }    
   }
+
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
 
-    let _userData = getUserData(accessToken);
-    if(_userData) this.userData = _userData;
-    console.log(this.userData);
+    getUserData(accessToken).then(data => this.setUserData(data));
+    console.log(this.state);
 
-    let _songData = getCurrentSongData(accessToken);
-    if(_songData) this.songData = _songData;
-    console.log(this.songData);
+    // let _songData = getCurrentSongData(accessToken);
+    // if(_songData) this.songData = _songData;
+    // console.log(this.songData);
 
     if(!accessToken) return; 
 
@@ -45,6 +47,15 @@ class App extends Component {
     // setInterval(() => {   
     //   this.update(accessToken);
     // }, 10000)
+  }
+
+  setUserData = (data) => {
+    console.log('Set User Data')
+    console.log(JSON.stringify(data));
+    this.setState({user_display_name: data.display_name});
+    this.setState({user_email: data.email});
+    this.setState({user_id: data.id});
+    this.setState({user_image_url: data.images[0].url});
   }
 
   update(accessToken){
@@ -66,18 +77,6 @@ class App extends Component {
       let search = encodeURI(this.state.songData.name + ' ' + this.state.songData.artists[0].name);
       this.GetUrlYoutube(search);
     }
-  }
-
-
-  GetCurrentSong(accessToken) {
-    console.log('Obteniendo la cancion Actual Spotify')
-    fetch('https://api.spotify.com/v1/me/player/currently-playing',{
-      headers: {'Authorization': 'Bearer ' + accessToken}
-    }).then(response => response.json())
-    .then(data => {
-      if(!data) return;
-      this.setState({songData: data.item});
-    });
   }
 
   GetUrlYoutube(search)
@@ -104,14 +103,13 @@ class App extends Component {
             <h1 style={{...defaultStyle, 'fontSize': '54px'}}>Vispotify</h1>
             <h2 style={{...defaultStyle, 'fontSize': '34px'}}>See your music</h2>
             <h3 style={{...defaultStyle, 'fontSize': '24px'}}>
-              Connected to {this.state.serverData.user.name} Account
+              Connected to {this.userData.display_name} Account
             </h3>
             <h4>Current Song: {this.state.songData.name}</h4>
             <h4>Artist: {this.state.songData.artists && this.state.songData.artists[0].name}</h4>
 
             {this.state.videoData.items ? 
             <YoutubePlayer videoId={this.state.videoData.items[0].id.videoId}/> : <h5>NoVideo</h5>}
-            
 
           </div> : <div>
               <button onClick={() => window.location = 'http://localhost:8888/login'}
--- src/SpotifyManager.js
+++ src/SpotifyManager.js
@@ -1,24 +1,24 @@
 export const getUserData = (accessToken) => {
     if(!accessToken) return null;
-    fetch('https://api.spotify.com/v1/me',
+    let _return = fetch('https://api.spotify.com/v1/me',
     {headers: {'Authorization': 'Bearer ' + accessToken}})
     .then(response => response.json())
     .then(data => {
-        console.log('getUserData('+accessToken+'):')
+        console.log('getUserData():')
         console.log(data);
         if(data.error) return null;
         return data;
     });
-    return null;          
+    return _return;          
 }
 
 export const getCurrentSongData = (accessToken) => {
     if(!accessToken) return null;
-    fetch('https://api.spotify.com/v1/me/player/currently-playing',
+    return fetch('https://api.spotify.com/v1/me/player/currently-playing',
     {headers: {'Authorization': 'Bearer ' + accessToken}})
     .then(response => response.json())
     .then(data => {
-        console.log('GetCurrentSongData('+accessToken+'):')
+        console.log('GetCurrentSongData():')
         console.log(data);
         if(data.error) return null;
         return data;
